0031353198f2ff9feca1e51ea9734eaf5df5a07c
Natalia Galán <n8galan@gmail.com>
2020-07-08 20:53:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,10 +7,10 @@ def load_library(path)
   # code goes here
  emoticons = YAML.load_file(path)
  emoticons_hash = {}
- emoticons.each do |english_meaning, array|
+ emoticons.each do |english_meaning, emoticon_array| #emoticon_array = ["O:)", "☜(⌒▽⌒)☞"]
    emoticons_hash[english_meaning] ||= {}
-   emoticons_hash[english_meaning][:english] = array[0]
-   emoticons_hash[english_meaning][:japanese] = array[1]
+   emoticons_hash[english_meaning][:english] = emoticon_array[0]
+   emoticons_hash[english_meaning][:japanese] = emoticon_array[1]
 binding.pry
  end
  emoticons_hash
@@ -20,7 +20,7 @@ binding.pry
 def get_japanese_emoticon(path, english_emoticon)
   # code goes here
   library = load_library(path)
-  library.each do |english_meaning, inner_hash| # english_meaning= "happy" inner_hash= {:english => ":)", :japanese => "(＾ｖ＾)"}
+  library.each do |english_meaning, inner_hash| # english_meaning = "happy" // inner_hash = {:english => ":)", :japanese => "(＾ｖ＾)"}
     return inner_hash[:japanese] if inner_hash[:english] == english_emoticon
   end
   "Sorry, that emoticon was not found"
